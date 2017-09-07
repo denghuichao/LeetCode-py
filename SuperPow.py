bb2ac729f27def6deedd675fd3c79fe8a7bd7d35
denghuichao <865871137@qq.com>
2017-09-07 23:53:15 +0800

    first commit

--- SuperPow.py
+++ SuperPow.py
@@ -5,3 +5,4 @@ class Solution(object):
         :type b: List[int]
         :rtype: int
         """
+        return 0 if a % 1337 == 0 else pow(a, reduce(lambda x, y: (x * 10 + y) % 1140, b) + 1140, 1337)
