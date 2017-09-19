143faed69ac8d482156085803b19bc6531c06c45
denghuichao <865871137@qq.com>
2017-09-19 19:09:58 +0800

    Non-decreasing Array

--- NonDecreasingArray.py
+++ NonDecreasingArray.py
@@ -9,3 +9,13 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
+        nums1, nums2 = nums[:], nums[:]
+        for i in range(len(nums) - 1):
+            if nums[i] > nums[i] + 1:
+                nums1[i] = nums1[i + 1]
+                nums2[i + 1] = nums2[i]
+
+        return nums1 == sorted(nums1) or nums2 == sorted(nums2)
+
+s = Solution()
+print(s.checkPossibility([1, 2, 4, 1,3]))
